a4fabb41a38530e3e9c1562599c804cec42d0bc1
dTomGehman <tomgehman1@gmail.com>
2026-03-16 10:58:37 -0400

    refactored tester, ready to add sorting functions in

--- src/tester.c
+++ src/tester.c
@@ -5,25 +5,46 @@
 
 #define BUFFREADSIZE 4096
 
+double process_file(int n_data, int n_files, short*buffer);
+
 int main(int argc, char**argv){
-
-    FILE *fp;
     short *buffer;
-    char filename[50];
 
-    char *filename_temp = "%s_%d_%03d.dat";
-    sprintf(filename, filename_temp, "uns", 5, 0);
-    printf("%s\n", filename);
+    int nos_data[] = {5, 10000, 100000, 1000000};
+    for (int index = 0; index < 4; index++) {
+        int n_data=nos_data[index];
+        buffer = malloc(sizeof(short) * n_data);
+        if (!buffer){ fprintf(stderr, "malloc fail \n"); exit(1);}
 
-    fp = fopen(filename, "r");
-    if (!fp) {fprintf(stderr, "failed to open %s\n", filename); exit(1);}
-    buffer = malloc(sizeof(short) * 5);
-
-    fread(buffer, sizeof(short), 5, fp);
-    for (int i=0; i<5; i++){
-        printf("%d\n", buffer[i]);
+        double time_taken;
+        for (int i=0; -1 != (time_taken = process_file(n_data, i, buffer)); i++)
+            printf("time taken %lf\n", time_taken);
+            
+        free(buffer);
     }
 
     return 0;
 }
 
+double process_file(int n_data, int file_no, short*buffer){
+    FILE *fp;
+    char filename[50];
+    sprintf(filename, "%s_%d_%03d.dat", "uns", n_data, file_no);
+    printf("%s\n", filename);
+
+    fp = fopen(filename, "r");
+    if (!fp) {printf("out of files size %d\n", n_data); return (-1);}
+
+    fread(buffer, sizeof(short), n_data, fp);
+
+
+    clock_t start = clock();
+    //put sorting function here
+    for (int i=0; i<n_data; i++){
+        printf("%d\n", buffer[i]);
+    }
+    clock_t end = clock();
+
+    fclose(fp);
+    return (double) (end - start) / CLOCKS_PER_SEC;
+}
