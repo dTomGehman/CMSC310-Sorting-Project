c044a40a267997b5d2cf890e1629060c2517edd7
dTomGehman <tomgehman1@gmail.com>
2026-03-21 13:52:37 -0400

    started readme.  also added print option to tester

--- src/tester.c
+++ src/tester.c
@@ -8,16 +8,16 @@
 #define BUFFREADSIZE 4096
 
 double process_file(int n_data, int file_no, char*unssorrev,
-        char*dir, char*salgo, short*buffer);
+        char*dir, char*salgo, short*buffer, int prince);
 
 
 int main(int argc, char**argv){
     short *buffer;
 
-    if (argc != 7) {
+    if (argc != 7 && argc != 8) {
         fprintf(stderr, "Usage: "
                 "./tester <dir> <uns|sor|rev> <n_data> <file_start>"
-                " \n\t\t\t<file_end> <selection|merge|quick>\n"
+                " \n\t\t\t<file_end> <selection|merge|quick> [print]\n"
 
                 "\nwhere   <dir> is directory of data files\n"
                 "\t<uns|sor|rev> indicates data arrangement "
@@ -26,6 +26,7 @@ int main(int argc, char**argv){
                 "\t<file_start> is the first file number to work with (usu. 0)\n"
                 "\t<file_end> is the last file number (usu. 29)\n"
                 "\t<selection|merge|quick> indicates what sorting algo to use\n"
+                "\tspecify print option to print out the buffer before/after sort\n"
                 "\n\nSorry for annoying usage; this makes it easier to script\n"
                );
         exit(1);
@@ -37,6 +38,8 @@ int main(int argc, char**argv){
     int file_start=atoi(argv[4]);
     int file_end=atoi(argv[5]);
     char*salgo = argv[6];
+    int prince=0;
+    if (argc==8 && !strcmp(argv[7], "print")) prince=1;
 
     buffer = malloc(sizeof(short) * n_data);
     //printf("n data %d\n", n_data);
@@ -44,7 +47,7 @@ int main(int argc, char**argv){
 
     double time_taken;
     for (int i=file_start; i<=file_end; i++)
-        printf(" %lf\n", process_file(n_data, i, unssorrev, dir, salgo, buffer));
+        printf(" %lf\n", process_file(n_data, i, unssorrev, dir, salgo, buffer, prince));
         
     free(buffer);
 
@@ -52,7 +55,7 @@ int main(int argc, char**argv){
 }
 
 double process_file(int n_data, int file_no, char*unssorrev,
-        char*dir, char*salgo, short*buffer){
+        char*dir, char*salgo, short*buffer, int prince){
     FILE *fp;
     char filename[50];
     sprintf(filename, "%s/%s_%d_%03d.dat", dir, unssorrev, n_data, file_no);
@@ -69,11 +72,11 @@ double process_file(int n_data, int file_no, char*unssorrev,
     }
     fread(buffer + pos, sizeof(short), n_data % BUFFREADSIZE, fp);
 
-    //if (argh > 1) {
-    //    printf("presorting");
-    //    for (int i=0; i<n_data; i++)
-    //        printf("%hu ", buffer[i]);
-    //}
+    if (prince) {
+        printf("presorting");
+        for (int i=0; i<n_data; i++)
+            printf("%hu ", buffer[i]);
+    }
     clock_t start = clock();
     //put sorting function here
     printf(" %s ", salgo);
@@ -84,12 +87,12 @@ double process_file(int n_data, int file_no, char*unssorrev,
     else if (!strcmp(salgo, "merge"))
         mergesort(buffer, n_data);
     clock_t end = clock();
-    //if (argh > 1) {
-    //    printf("\n");
-    //    printf("sorted");
-    //    for (int i=0; i<n_data; i++)
-    //        printf("%hu ", buffer[i]);
-    //}
+    if (prince) {
+        printf("\n");
+        printf("sorted");
+        for (int i=0; i<n_data; i++)
+            printf("%hu ", buffer[i]);
+    }
 
     fclose(fp);
     return (double) (end - start) / CLOCKS_PER_SEC * 1000;
